662161e55b0aa4c47c33a589114d465ffa2b0e49
hasan-naim <hasannaim6246@gmail.com>
2022-11-08 21:48:38 +0600

    initialized the project and made the first api

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+/// middlewares
+
+app.use(cors());
+app.use(express.json());
+
+/// mongodb connect
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.49zsx7x.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+// });
+async function dbConnect() {
+  try {
+    const database = client.db("yourKitch");
+
+    /// database collection
+    const mealsCollection = database.collection("meals");
+
+    /// get apis
+
+    app.get("/meals", async (req, res) => {
+      const query = {};
+      const cursor = await mealsCollection.find({});
+      const result = await cursor.sort({ time: -1 }).toArray();
+      res.send({ status: 400, meals: result });
+    });
+  } catch (error) {}
+}
+dbConnect().catch((err) => console.log(err));
+app.listen(port, () => console.log(`server is listening on ${port}`));
