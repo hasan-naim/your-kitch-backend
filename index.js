0a6444128b4b7e85b93e40a3b69399de8cd616a7
hasan-naim <hasannaim6246@gmail.com>
2022-11-10 17:31:00 +0600

    jwt token api  is created

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
@@ -11,6 +12,28 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const verifyJwt = (req, res, next) => {
+  let token = req.headers.authorization;
+  if (!token) {
+    res.status(401).send({ message: "unauthorized user" });
+    return;
+  }
+  token = token.split(" ")[1];
+  const decoded = jwt.verify(
+    token,
+    process.env.ACCESS_TOKEN_SECRET,
+    (err, decoded) => {
+      if (err) {
+        res.status(403).send({ message: "unauthorized access" });
+        return;
+      }
+
+      req.decoded = decoded;
+      next();
+    }
+  );
+};
 
 /// mongodb connect
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.49zsx7x.mongodb.net/?retryWrites=true&w=majority`;
@@ -29,6 +52,15 @@ async function dbConnect() {
     /// database collection
     const mealsCollection = database.collection("meals");
     const reviewCollection = database.collection("reviews");
+    /// JWT TOKEN api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log("jwt");
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "7d",
+      });
+      res.send({ status: 200, token });
+    });
 
     /// get apis
 
@@ -43,7 +75,7 @@ async function dbConnect() {
         cursor = await mealsCollection.find({}).limit(size);
       }
       const result = await cursor.sort({ time: -1 }).toArray();
-      res.send({ status: 400, meals: result });
+      res.send({ status: 200, meals: result });
     });
 
     app.get("/meals/:id", async (req, res) => {
@@ -51,7 +83,7 @@ async function dbConnect() {
       const query = { _id: ObjectId(id) };
       const cursor = mealsCollection.find(query);
       const meal = await cursor.toArray();
-      res.send({ status: 400, meal: meal });
+      res.send({ status: 200, meal: meal });
     });
 
     /// get reviews by id
@@ -60,21 +92,28 @@ async function dbConnect() {
       const query = { mealId: id };
       const cursor = reviewCollection.find(query);
       const result = await cursor.sort({ time: -1 }).toArray();
-      res.send({ status: 400, reviews: result });
+      res.send({ status: 200, reviews: result });
     });
     app.get("/reviewtoedit/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await reviewCollection.findOne(query);
-      res.send({ status: 400, review: result });
+      res.send({ status: 200, review: result });
     });
     /// by email
-    app.get("/reviewsByEmail", async (req, res) => {
+    app.get("/reviewsByEmail", verifyJwt, async (req, res) => {
+      const decoded = req.decoded;
+
       const email = req.query.email;
+
+      if (decoded.email !== email) {
+        return res.status(403).send({ message: "unauthorized access" });
+      }
+
       const query = { email: email };
       const cursor = reviewCollection.find(query);
       const result = await cursor.sort({ time: -1 }).toArray();
-      res.send({ status: 400, reviews: result });
+      res.send({ status: 200, reviews: result });
     });
 
     ///post apies
@@ -103,7 +142,7 @@ async function dbConnect() {
       };
 
       const result = await reviewCollection.updateOne(query, updatedData);
-      res.send({ status: 400, result: result });
+      res.send({ status: 200, result: result });
     });
 
     /// delete
@@ -113,7 +152,7 @@ async function dbConnect() {
       const query = { _id: ObjectId(id) };
 
       const result = await reviewCollection.deleteOne(query);
-      res.send({ status: 400, result: result });
+      res.send({ status: 200, result: result });
     });
 
     /// handaling the error
